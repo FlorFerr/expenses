dac6bf3efc84e3053cbbce093b09cb8c5ac6a7d8
Flor <florferreyra16@gmail.com>
2022-04-27 09:14:06 -0300

    carga de archivos

---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
+
+const Expenses = ({ items }) => {
+
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter = {filterHandler} />
+
+      {items.length > 0 ?
+
+    items.map(i =><ExpenseItem key={i.id}title={i.title} amount={i.amount} date={i.date}/>) 
+    : <h2>No hay gastos</h2>
+    
+      }
+    </Card>
+   
+  )
+}
+
+export default Expenses
